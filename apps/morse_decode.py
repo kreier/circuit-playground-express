9be2706d93cef13b6173747eea1f703d39c01bec
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2023-04-19 22:47:07 +0700

    working example with 30ms pulsewidth

--- apps/morse_decode.py
+++ apps/morse_decode.py
@@ -2,13 +2,14 @@
 
 import board, analogio, time
 
-THRESHOLD = 3000
-DOT = 0.0019
+THRESHOLD = 1000
+DOT = 0.028
 DASH = 3 * DOT
 SPACE = 7 * DOT
 average = 0
 last_values = []
 last_index = 0
+light_value = 0
 old_value = 0
 morse = []
 letters = {"A":[1,3],"B":[3,1,1,1],"C":[3,1,3,1],"D":[3,1,1],"E":[1],"F":[3,1,1,1],"G":[3,3,1],"H":[1,1,1,1],"I":[1,1],"J":[1,3,3,3],"K":[3,1,3],"L":[1,3,1,1],"M":[3,3],"N":[3,1],"O":[3,3,3],"P":[1,3,3,1],"Q":[3,3,1,3],"R":[1,3,1],"S":[1,1,1],"T":[3],"U":[1,1,3],"V":[1,1,1,3],"W":[1,3,3],"X":[3,1,1,3],"Y":[3,1,3,3],"Z":[3,3,1,1]}
@@ -35,31 +36,42 @@ def get_average(light_raw):
     average /= len(last_values)
     return average
 
+def letter(morselist):
+    try:
+        a = list(letters.values()).index(morselist)
+    except:
+        a = 0
+    return list(letters.keys())[a]
+
 while True:
     light = lightsensor.value
     a = get_average(light)
     if light > average + THRESHOLD:
         light_value = 1
-    else:
+    elif light < average - THRESHOLD:
         light_value = 0
     #print(light_value, end='')
     #print("(", light, ", ", average, ")")
     if light_value != old_value:
         if light_value == 1:
             starttime = time.monotonic()
+            blanktime = starttime - stoptime
             # end of letter - 3 dots or 1 dash
-            if starttime - stoptime > DASH:
+            if blanktime > DASH:
                 # decode the letter
-                print(morse, end=' ')
-                print("A", end='')
+                print(letter(morse), end='')
                 morse = []
+                if blanktime > SPACE:
+                    print(" ", end='')
+
         else:
             stoptime = time.monotonic()
             pulsetime = stoptime - starttime
+            #print(pulsetime)
             if pulsetime > DOT:
                 if pulsetime > DASH:
                     morse.append(3)
                 else:
                     morse.append(1)
     old_value = light_value
-    time.sleep(0.001)
+    time.sleep(0.0005)
